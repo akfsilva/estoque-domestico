676cc4f871162df7576453302998405bf8990daf
akfsilva <alinekfs@protonmail.com>
2025-12-26 00:29:11 -0300

    Refactor service worker for caching and asset management

--- sw.js
+++ sw.js
@@ -1,13 +1,34 @@
-﻿self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("estoque-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js"
-      ]);
+const CACHE_NAME = "estoque-lofi-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
-
 });
